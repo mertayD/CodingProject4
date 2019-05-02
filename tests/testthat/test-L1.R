40ce9dcf688a2fb93e88541ccb61bc38226b81be
Tyler Thatcher <tthatcher84@gmail.com>
2019-05-02 14:11:16 -0700

    Updated with travis and tests

---
+++ tests/testthat/test-L1.R
@@ -0,0 +1,12 @@
+# write tests for that R function, in tests/testthat/test-NNetIterations:
+# (1) for valid inputs your function returns an output of the expected type/dimension
+# (2) for an invalid input, your function stops with an informative error message.
+
+library(CodingProject4)
+library(testthat)
+context("test-L1")
+
+
+test_that("Dummy test", {
+  return TRUE
+})
